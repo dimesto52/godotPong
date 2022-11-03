9479e28d9282b7c980ef884adb318f86245e4271
dimesto52 <98359317+dimesto52@users.noreply.github.com>
2022-11-03 14:05:27 +0100

    ep 02 la balle

---
+++ ballMove.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class ballMove : Node2D
+{
+
+    [Export]
+    private Vector2 direction;
+    [Export]
+    private float speed = 20.0f;
+
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Position = Vector2.Zero;
+
+        RandomNumberGenerator rnd = new RandomNumberGenerator();
+
+        direction = new Vector2(rnd.RandfRange(-1.0f,1.0f),rnd.RandfRange(-1.0f,1.0f)).Normalized();
+
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+      Position += direction * delta * speed;
+  }
+}
